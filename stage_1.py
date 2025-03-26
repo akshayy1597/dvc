92be85cedf2e654a8dd48f49c6788b91d4cb0fa1
akshayy1597 <60820078+akshayy1597@users.noreply.github.com>
2025-03-25 20:28:55 -0400

    updated dvc yaml

---
+++ stage_1.py
@@ -0,0 +1,2 @@
+with open("artifact.txt","w") as f:                 # open file
+    f.write("my name is sunny stage one is done")   # write to file
---
+++ stage_2.py
@@ -0,0 +1,6 @@
+with open("artifact.txt","r") as f:     # open file with r as read mode
+    text=f.read()                       # read from file
+    
+print(text)
+print("it is a end of stage 2")
+print("i am going to add stage 3")
